036e8ce64bd2531d75621cf4ed2f28ebba58c331
Tim Morgan <tim@timmorgan.org>
2023-08-22 19:35:04 -0500

    Reorganize files and add bin/mya

---
+++ lib/mya.rb
@@ -0,0 +1,3 @@
+require_relative './mya/compiler'
+require_relative './mya/jit'
+require_relative './mya/vm'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,5 @@ require 'minitest/reporters'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-require_relative '../lib/compiler'
-require_relative '../lib/jit'
-require_relative '../lib/vm'
+require_relative '../lib/mya'
 require_relative '../spec/support/expectations'
